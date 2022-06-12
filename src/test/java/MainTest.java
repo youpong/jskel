85211712f0123459cef1835d8952df7ea68265c0
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-06-12 17:32:20 +0900

    init

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
