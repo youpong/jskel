daa85a3e15603074f45759601c955d2eeface5f9
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-06-15 16:19:03 +0900

    add @BeforeAll

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,13 @@
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 public class MainTest {
+    @BeforeAll
+    public static void setup() {
+    }
+
     @Test
     public void test() {
         assertTrue(true);
