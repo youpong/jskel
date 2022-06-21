fbac7a72f35cab10a903e91bc3979d17f5293aa0
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-06-21 17:49:44 +0900

    wip

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,10 +2,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class MainTest {
     @BeforeAll
-    public static void setup() {
+    public static void setupAll() {
+    }
+
+    @BeforeEach
+    public void setup() {
     }
 
     @Test
